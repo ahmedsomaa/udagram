42c95bd8990d8ebe1e184a4d3ba306051175cfa7
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-11-01 20:12:40 +0200

    refactor: update to use circle ci secrets

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://udagram-client/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram-client/
+aws s3 cp --recursive --acl public-read ./www $AWS_ANGULAR_BUCKET
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html $AWS_ANGULAR_BUCKET
